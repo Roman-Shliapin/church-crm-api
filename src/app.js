9ad15971f9d395c264ebaeb7f704718789dfc556
Roman-Shliapin <romashlaypin@gmail.com>
2026-04-23 21:25:44 +0200

    feat(auth): add register endpoint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js'
+
+
+
 dotenv.config()
+
+
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 // test route
 app.get("/", (req, res) => {
---
+++ src/controllers/authController.js
@@ -0,0 +1,42 @@
+import Member from '../models/Member.js';
+import Candidate from '../models/Candidate.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+export const register = async (req, res) => {
+    const { phone, email, password } = req.body;
+
+    try {
+        // Ищем пользователя по номеру телефона в обеих коллекциях
+        let user = await Member.findOne({ phone });
+        let role = 'user';
+
+        if (!user) {
+            user = await Candidate.findOne({ phone });
+        };
+
+        if (!user) {
+            return res.status(404).json({ message: 'Користувача з таким номером не знайдено' });
+        };
+
+        if (user.password) {
+            return res.status(400).json({ message: 'Акаунт вже зареєстровано' });
+        };
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        user.email = email
+        user.password = hashedPassword
+        await user.save()
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role ?? role },
+            process.env.JWT_SECRET,
+            { expiresIn: '7d' },
+        );
+
+        res.status(201).json({ token, name: user.name, role: user.role ?? role })
+    } catch (error) {
+        res.status(500).json({ message: 'Помилка сервера', error: err.message });
+    }
+};
