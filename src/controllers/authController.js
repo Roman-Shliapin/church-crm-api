00cd5c7bf0b63e38e64f4b2b0f948f12c7f0af03
Roman-Shliapin <romashlaypin@gmail.com>
2026-04-23 21:36:55 +0200

    feat(auth): add login endpoint

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -40,3 +40,42 @@ export const register = async (req, res) => {
         res.status(500).json({ message: 'Помилка сервера', error: err.message });
     }
 };
+
+
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        let user = await Member.findOne({ email });
+        let role = 'user'
+
+        if (!user) {
+            user = await Candidate.findOne({ email });
+        };
+
+        if (!user) {
+            return res.status(404).json({ message: 'Користувача не знайдено' })
+        }
+
+        if (!user.password) {
+            return res.status(400).json({ message: 'Акаунт ще не активовано. Пройдіть реєстрацію' })
+        };
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Невірний пароль' });
+        };
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role ?? role },
+            process.env.JWT_SECRET,
+            { expiresIn: '7d' },
+        );
+
+        res.json({ token, name: user.name, role: user.role ?? role });
+    } catch (error) {
+        res.status(500).json({ message: 'Помилка сервера', error: err.message });
+    }
+}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
 
 
 const router = Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 
 export default router;
